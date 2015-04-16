aedc8f36fecc4770ed4b9d3ac59a6b513a5b5482
MichaelAMA <joebob60@hotmail.com>
2015-04-15 18:58:43 -0700

    Add all the CSharp files and directories.

---
+++ CSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+           
+            PrintHello("Michael");
+        }
+    // testing git
+        static void PrintHello(string name)
+        {
+            Console.WriteLine("Hello, {0}", name);
+        }
+
+    }
+}
